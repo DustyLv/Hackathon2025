b0d5e7b81449cc5c613e1cefd353fd90f667943f
zintiserics <zintis.erics@outlook.com>
2025-06-15 06:18:17 +0300

    Fix.

--- Assets/_Hackathon2025/Scripts/IKController.cs
+++ Assets/_Hackathon2025/Scripts/IKController.cs
@@ -46,11 +46,11 @@ public class IKController : MonoBehaviour
 
         IKTarget_RightHand.localPosition = new Vector3(right.x, right.y, -right.z);
         IKTarget_LeftHand.localPosition = new Vector3(left.x, left.y, -left.z);
-        // IKTarget_Head.localPosition = new Vector3(head.x, head.y, head.z);
+        IKTarget_Head.localPosition = new Vector3(head.x, head.y, -head.z);
 
         IKTarget_RightHand.localRotation = Quaternion.Euler(-rightR.eulerAngles.x, -rightR.eulerAngles.y, rightR.eulerAngles.z);
         IKTarget_LeftHand.localRotation = Quaternion.Euler(-leftR.eulerAngles.x, -leftR.eulerAngles.y, leftR.eulerAngles.z);
-        // IKTarget_Head.localRotation = Quaternion.Euler(headR.eulerAngles.x, headR.eulerAngles.y, headR.eulerAngles.z);
+        IKTarget_Head.localRotation = Quaternion.Euler(-headR.eulerAngles.x, headR.eulerAngles.y, headR.eulerAngles.z);
     }
 
     public void StopPossessedState()
